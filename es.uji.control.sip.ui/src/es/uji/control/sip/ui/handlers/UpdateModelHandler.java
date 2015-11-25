bd25734a71f4aaa3fa487f213634ecc945670659
tjulian <tjulian@uji.es>
2015-11-25 10:14:46 +0100

    Implementando el handler de actualización.

--- es.uji.control.sip.ui/src/es/uji/control/sip/ui/handlers/UpdateModelHandler.java
+++ es.uji.control.sip.ui/src/es/uji/control/sip/ui/handlers/UpdateModelHandler.java
@@ -1,10 +1,15 @@
 package es.uji.control.sip.ui.handlers;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
@@ -35,41 +40,65 @@ public class UpdateModelHandler {
 			this.shell = shell;
 		}
 	}
-	
+
 	@Execute
 	public void execute() {
-		
-		IControlConnection connection = null;
-		
-		try {
-			connection = connectionFactory.createConnection();
-			
-			connection.getPersonService().getAllPersons(new IPersonStream() {
-				
-				@Override
-				public void onNext(List<IPerson> persons) {
+
+		Job job = new Job("Carga del modelo") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					IControlConnection connection = connectionFactory.createConnection();
+					
 					System.out.println("-------------------------------------------------------");
-					System.out.println(persons.size());
+					System.out.println(String.format("Empieza la carga del modelo: %s", LocalDateTime.now().toString()));
 					System.out.println("-------------------------------------------------------");
+					
+					connection.getPersonService().getAllPersons(new IPersonStream() {
+					int numPersons = 0;
+						
+						@Override
+						public void onNext(List<IPerson> persons) {
+							int size = persons.size();
+							System.out.println("-------------------------------------------------------");
+							System.out.println(size);
+							System.out.println(String.format("DNI:%s, Nombre:%s, Apellidos:%s %s ",
+									persons.get(size-1).getIdentification(),
+									persons.get(size-1).getName(),
+									persons.get(size-1).getFirstLastName(),
+									persons.get(size-1).getSecondLastName()));
+							System.out.println("-------------------------------------------------------");
+							numPersons = numPersons + size;
+						}
+
+						@Override
+						public void onError(Throwable t) {
+							System.out.println("----------------------ERROR----------------------------");
+						}
+
+						@Override
+						public void onCompleted() {
+							System.out.println("-------------------------------------------------------");
+							System.out.println(String.format("Finaliza la carga del modelo: %s", LocalDateTime.now().toString()));
+							System.out.println(String.format("Actualmente se han cargado %d personas", numPersons));
+							System.out.println("-------------------------------------------------------");
+						}
+					});
+
+				} catch (ControlConnectionException e) {
+					MessageDialog.openError(shell, "Error", e.getMessage());
+					return Status.CANCEL_STATUS;
+				} catch (ControlNotImplementedException e) {
+					MessageDialog.openError(shell, "Error", e.getMessage());
+					return Status.CANCEL_STATUS;
 				}
-				
-				@Override
-				public void onError(Throwable t) {
-					System.out.println("----------------------ERROR---------------------------------");
-				}
-				
-				@Override
-				public void onCompleted() {
-					System.out.println("--------------------------MODELO CARGADO---------------------");
-				}
-			});
-			
-		} catch (ControlConnectionException e) {
-			MessageDialog.openError(shell, "Error", e.getMessage());
-		} catch (ControlNotImplementedException e) {
-			MessageDialog.openError(shell, "Error", e.getMessage());
-		}
-		
+
+				return Status.OK_STATUS;
+			}
+
+		};
+		job.schedule();
+
 	}
 
 	@CanExecute
@@ -79,14 +108,16 @@ public class UpdateModelHandler {
 
 	@Inject
 	@Optional
-	private void subscribeConnectionFactoryAdded(@UIEventTopic("ADDED_CONNECTION_FACTORY_SERVICE") IControlConnectionFactory connectionFactory) {
+	private void subscribeConnectionFactoryAdded(
+			@UIEventTopic("ADDED_CONNECTION_FACTORY_SERVICE") IControlConnectionFactory connectionFactory) {
 		this.connectionFactory = connectionFactory;
 		this.canActivate = true;
 	}
 
 	@Inject
 	@Optional
-	private void subscribeConnectionFactoryRemoved(@UIEventTopic("REMOVED_CONNECTION_FACTORY_SERVICE") IControlConnectionFactory connectionFactory) {
+	private void subscribeConnectionFactoryRemoved(
+			@UIEventTopic("REMOVED_CONNECTION_FACTORY_SERVICE") IControlConnectionFactory connectionFactory) {
 		this.canActivate = false;
 		this.connectionFactory = null;
 	}
