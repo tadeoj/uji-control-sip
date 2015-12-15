b53d2a5e75ddf771c1cbb5e5381e8ea39a6efc0b
tjulian <tjulian@uji.es>
2015-12-15 09:37:31 +0100

    Refactorización

---
+++ es.uji.control.sip.ui.preferences/src/es/uji/control/sip/ui/preferences/internal/SIPPreferencesComponent.java
@@ -0,0 +1,47 @@
+package es.uji.control.sip.ui.preferences.internal;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
+import es.uji.control.domain.provider.service.factoryselector.IControlConnectionFactorySelector;
+
+@Component(name = "sip.ui.preferences", immediate = true)
+public class SIPPreferencesComponent {
+
+	private BundleContext bundlecontext;
+	static private IControlConnectionFactorySelector factorySelector;
+
+	@Activate
+	public void activate(ComponentContext componentContext) throws Exception {
+		this.bundlecontext = componentContext.getBundleContext();
+	}
+
+	@Deactivate
+	public void deactivate(ComponentContext componentContext) {
+		this.bundlecontext = null;
+	}
+
+	public BundleContext getContext() {
+		return this.bundlecontext;
+	}
+
+	@Reference(policy = ReferencePolicy.DYNAMIC, cardinality = ReferenceCardinality.OPTIONAL, name = "sipControlConnectionFactorySelector")
+	public void bindConnectionFactorySelector(IControlConnectionFactorySelector factorySelector) {
+		this.factorySelector = factorySelector;
+	}
+
+	public void unbindConnectionFactorySelector(IControlConnectionFactorySelector factorySelector) {
+		this.factorySelector = null;
+	}
+
+	public static IControlConnectionFactorySelector getSelector() {
+		return factorySelector;
+	}
+
+}
---
+++ es.uji.control.sip.ui.preferences/src/es/uji/control/sip/ui/preferences/internal/SIPSelectorPreferencesPage.java
@@ -0,0 +1,78 @@
+package es.uji.control.sip.ui.preferences.internal;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.jface.preference.ComboFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+
+import es.uji.control.domain.provider.service.factoryselector.ConnectionFactoryKey;
+import es.uji.control.domain.provider.service.factoryselector.IControlConnectionFactorySelector;
+
+
+public class SIPSelectorPreferencesPage extends FieldEditorPreferencePage {
+
+	private IControlConnectionFactorySelector selector;
+	private ComboFieldEditor combo;
+	
+	public SIPSelectorPreferencesPage() {
+		super(GRID);
+	}
+	
+	@Override
+	protected void createFieldEditors() {
+		
+		String[][] connections = new String[][]{};
+		selector = SIPPreferencesComponent.getSelector();
+		
+		if (selector != null) {
+			Set<ConnectionFactoryKey> factoryKeys = selector.getFactoryKeys();
+			
+			ConnectionFactoryKey[] array = factoryKeys.toArray(new ConnectionFactoryKey[factoryKeys.size()]);
+			connections = new String[factoryKeys.size()][2];
+						
+			for (int i = 0; i < array.length; i++) {
+				connections[i][0] = selector.getFactoryDescription(array[i]);
+				connections[i][1] = array[i].toString();
+			}
+		}
+		
+		combo = new ComboFieldEditor("prefCombo", "Elija una conexión:", connections, getFieldEditorParent());
+		addField(combo);
+
+	}
+
+	//TODO: Investigar comportamiento 
+	public String getSelectedValue() { 
+		return combo.getPreferenceStore().getString(combo.getPreferenceName());
+	}
+	
+	public ConnectionFactoryKey getConnectionFactoryKey(String value) {
+	
+		if (selector != null) {
+			Set<ConnectionFactoryKey> factoryKeys = selector.getFactoryKeys();
+			
+			Iterator<ConnectionFactoryKey> iterator = factoryKeys.iterator();
+			
+			while (iterator.hasNext()) {
+				ConnectionFactoryKey key = iterator.next();
+				if (key.toString().equals(value)) {
+					return key;
+				}
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public boolean performOk() {
+		boolean result = super.performOk();
+		String selected = getSelectedValue();
+ 		ConnectionFactoryKey key = getConnectionFactoryKey(selected);
+		if (key != null) {
+			selector.setCurrentFactoryKey(key);
+		}
+		return result;
+	}
+
+}
