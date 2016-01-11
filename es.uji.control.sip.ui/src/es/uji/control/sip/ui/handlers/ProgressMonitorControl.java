fd56107418e77f08ad456db99c99e636e4ab17c3
tjulian <tjulian@uji.es>
2016-01-11 14:42:55 +0100

    Implementando el status bar.

--- es.uji.control.sip.ui/src/es/uji/control/sip/ui/handlers/ProgressMonitorControl.java
+++ es.uji.control.sip.ui/src/es/uji/control/sip/ui/handlers/ProgressMonitorControl.java
@@ -1,6 +1,9 @@
 package es.uji.control.sip.ui.handlers;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -16,30 +19,57 @@ import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.ProgressBar;
+
+import es.uji.control.model.sip.IModel;
 
 public class ProgressMonitorControl {
 
 	private final UISynchronize sync;
 
+	private final IModel model;
+
 	private ProgressBar progressBar;
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 	private GobalProgressMonitor monitor;
 
 	@Inject
-	public ProgressMonitorControl(UISynchronize sync) {
+	public ProgressMonitorControl(UISynchronize sync, IModel model) {
 		this.sync = Objects.requireNonNull(sync);
+		this.model = model;
 	}
 
 	@PostConstruct
 	public void createControls(Composite parent) {
-		
+
 		final Composite comp = new Composite(parent, SWT.NONE);
-		comp.setLayout(new GridLayout());
-		
+		comp.setLayout(new GridLayout(2, false));
+		comp.setSize(SWT.DEFAULT, SWT.DEFAULT);
+
+		Label label = new Label(comp, SWT.NONE);
+		GridDataFactory.fillDefaults().hint(400, SWT.DEFAULT).applyTo(label);
+
 		progressBar = new ProgressBar(comp, SWT.SMOOTH);
 		progressBar.setBounds(100, 10, 200, 20);
-		GridDataFactory.fillDefaults().grab(true, true).align(SWT.END, SWT.CENTER).applyTo(progressBar);
-		
+		GridDataFactory.fillDefaults().align(SWT.END, SWT.CENTER).applyTo(progressBar);
+
+		if (model != null) {
+			model.setUpdateModelStateTracker(new Consumer<LocalDateTime>() {
+
+				@Override
+				public void accept(LocalDateTime t) {
+					sync.asyncExec(new Runnable() {
+
+						@Override
+						public void run() {
+							label.setText(String.format("Modelo cargado: %s", t.format(formatter)));
+						}
+					});
+				}
+			});
+		}
+
 		monitor = new GobalProgressMonitor();
 
 		Job.getJobManager().setProgressProvider(new ProgressProvider() {
@@ -110,39 +140,5 @@ public class ProgressMonitorControl {
 			return this;
 		}
 	}
-
-	// private DateTimeFormatter formatter =
-	// DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-
-	// @PostConstruct
-	// public void createGui(Composite parent, IModel modelSIP) {
-	//
-	// this.model = modelSIP;
-	//
-	// final Composite comp = new Composite(parent, SWT.NONE);
-	// comp.setLayout(new GridLayout());
-	// Label label = new Label(comp, SWT.NONE);
-	// GridDataFactory.fillDefaults().hint(600, SWT.DEFAULT).applyTo(label);
-	//
-	// progressBar = new ProgressBar(comp, SWT.INDETERMINATE);
-	// GridDataFactory.fillDefaults().hint(200, 10).align(SWT.RIGHT,
-	// SWT.CENTER).applyTo(progressBar);
-	//
-	// if (model != null) {
-	// model.setUpdateModelStateTracker(new Consumer<LocalDateTime>() {
-	//
-	// @Override
-	// public void accept(LocalDateTime t) {
-	// sync.asyncExec(new Runnable() {
-	//
-	// @Override
-	// public void run() {
-	// label.setText(String.format("Modelo cargado: %s", t.format(formatter)));
-	// }
-	// });
-	// }
-	// });
-	// }
-	// }
 
 }
